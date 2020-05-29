db539f5071d17a1fb6807cb28dfbaa671709abf4
Robert Hunger <robert.hunger@mode-it-systems.de>
2020-05-29 18:43:04 +0000

    with pic and log

--- Rules/EmailValidation.js
+++ Rules/EmailValidation.js
@@ -4,6 +4,12 @@ export default function EmailValidation(atSign) {
 	if ((atSign.evaluateTargetPath('#Page:CustomerEditPage/#Control:FCEmail/#Value').indexOf('@')) === -1) {
 
 		//If email value does not contain @ display a validation failure message to the end-user
+		
+		//Not 2584129: How to add custom beep sound to Fiori Client/Kapsel application
+		var url = "http://soundbible.com/mp3/Bleep-SoundBible.com-1927126940.mp3";
+		var audio = new Audio(url);
+		audio.play();
+
 		atSign.executeAction('/RobSampleApp1/Actions/ValidationFailure.action');
 	} else {
 
